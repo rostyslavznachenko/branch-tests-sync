c2608bc6302be86f859478802a4dc2923ce3d3e3
rostyslav znachenko <rostislav.znachenko@gmail.com>
2023-09-18 20:22:13 +0100

    comments

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const { getTests } = require('find-cypress-specs')
-const { execSync } = require('child_process');
+const { execSync } = require('child_process')
 const { collectTests, getDifferenceBetweenBranches } = require('../src/testmanager.js')
+const arg = require('arg')
 const fs = require('fs')
 
-const TEMP_FILE_NAME = 'temp.spec.js'
+const args = arg({
+    '--branch': string
+})
+
+const remoteBranch = arsg['--branch'] || main
+const tempSpecFile = 'temp.spec.js'
 
 const currentBranch = execSync('git branch --show-current').toString()
 const changedSpecs = execSync('npx find-cypress-specs --branch main')
@@ -13,32 +19,39 @@ const changedSpecs = execSync('npx find-cypress-specs --branch main')
     .filter(Boolean)
 if (!changedSpecs)
 {
+    // add debug line here
     console.log(`There are no changed spec files in the current branch: ${currentBranch}`)
 }
 
+// print spec files
 console.log(changedSpecs)
+
+// debug git
 execSync('git fetch origin main:main')
 const results = {}
 changedSpecs.forEach(changedSpec =>
 {
     const { jsonResults, tagTestCounts } = getTests([changedSpec])
 
+    // debug git
     const checkIfSpecExists = execSync(`git ls-tree -r main --name-only | grep ${changedSpec} || true`).toString()
     if (checkIfSpecExists)
     {
+        // debug
         console.log(`Spec file ${changedSpec} exist on remote. Comparing results`)
         const tempRemote = execSync(`git show main:${changedSpec}`).toString()
 
-        fs.writeFileSync(TEMP_FILE_NAME, tempRemote, 'utf8')
-        const { jsonResults: remoteJsonResults, tagTestCounts: remoteTagTestCounts } = getTests([TEMP_FILE_NAME])
-
+        // debug
+        fs.writeFileSync(tempSpecFile, tempRemote, 'utf8')
+        const { jsonResults: remoteJsonResults, tagTestCounts: remoteTagTestCounts } = getTests([tempSpecFile])
 
         const diff = getDifferenceBetweenBranches(
-            collectTests(remoteJsonResults[TEMP_FILE_NAME].tests),
+            collectTests(remoteJsonResults[tempSpecFile].tests),
             collectTests(jsonResults[changedSpec].tests))
         results[changedSpec] = diff
     } else
     {
+        // debug
         console.log(`Spec file doesn't exist on remote. Don't need to compare results with remote as we need to run whole spec. Spec file ${changedSpec}`)
         results[changedSpec] = collectTests(jsonResults[changedSpec].tests)
     }
@@ -46,10 +59,13 @@ changedSpecs.forEach(changedSpec =>
 
 try
 {
-    fs.unlinkSync(TEMP_FILE_NAME);
-    console.log(`File "${TEMP_FILE_NAME}" has been deleted.`);
+    // debug
+    fs.unlinkSync(tempSpecFile);
+    console.log(`File "${tempSpecFile}" has been deleted.`);
 } catch (err)
 {
-    console.error(`Error deleting file "${TEMP_FILE_NAME}":`, err);
+    console.error(`Error deleting file "${tempSpecFile}":`, err);
 }
+
+// debug - writing to file?
 console.log(JSON.stringify(results, null, 2))
