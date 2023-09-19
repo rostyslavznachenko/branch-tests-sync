289a76820479b91208797d6095fa9410bdeacbbf
rostyslav znachenko <rostislav.znachenko@gmail.com>
2023-09-19 22:07:16 +0100

    Adding debug

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const { getTests } = require('find-cypress-specs')
 const { execSync } = require('child_process')
 const { collectTests, getDifferenceBetweenBranches } = require('../src/testmanager.js')
+const debugGit = (debug)('branch-sync:git')
+const debug = (debug)('branch-sync')
 const arg = require('arg')
 const fs = require('fs')
 
@@ -12,36 +14,35 @@ const remoteBranch = arsg['--branch'] || main
 const tempSpecFile = 'temp.spec.js'
 
 const currentBranch = execSync('git branch --show-current').toString()
-const changedSpecs = execSync('npx find-cypress-specs --branch main')
+const changedSpecs = execSync(`npx find-cypress-specs --branch ${remoteBranch}`)
     .toString()
     .trim()
     .split(',')
     .filter(Boolean)
 if (!changedSpecs)
 {
-    // add debug line here
-    console.log(`There are no changed spec files in the current branch: ${currentBranch}`)
+    debug('There are no changed spec files in the current branch %s', currentBranch)
+    debug('Exiting...')
+    process.exit(0)
 }
 
-// print spec files
-console.log(changedSpecs)
+debug('Changed spec files are %s', changedSpecs)
 
 // debug git
-execSync('git fetch origin main:main')
+debugGit('Fetching changes from the remote repository \'%s\' and updates the local main branch with those changes.', remoteBranch)
+execSync(`git fetch origin ${remoteBranch}:main`)
 const results = {}
 changedSpecs.forEach(changedSpec =>
 {
     const { jsonResults, tagTestCounts } = getTests([changedSpec])
 
-    // debug git
+    debugGit('Checking if %s exist on remote repository', changedSpec)
     const checkIfSpecExists = execSync(`git ls-tree -r main --name-only | grep ${changedSpec} || true`).toString()
     if (checkIfSpecExists)
     {
-        // debug
-        console.log(`Spec file ${changedSpec} exist on remote. Comparing results`)
+        debug('Spec file %s exist on remote repository. Saving remote version and comparing with local')
         const tempRemote = execSync(`git show main:${changedSpec}`).toString()
 
-        // debug
         fs.writeFileSync(tempSpecFile, tempRemote, 'utf8')
         const { jsonResults: remoteJsonResults, tagTestCounts: remoteTagTestCounts } = getTests([tempSpecFile])
 
@@ -51,21 +52,19 @@ changedSpecs.forEach(changedSpec =>
         results[changedSpec] = diff
     } else
     {
-        // debug
-        console.log(`Spec file doesn't exist on remote. Don't need to compare results with remote as we need to run whole spec. Spec file ${changedSpec}`)
+        debug('Spec file %s doesn\'t exist on remote. Don\'t need to compare results with remote as we need to run the whole spec', changedSpec)
         results[changedSpec] = collectTests(jsonResults[changedSpec].tests)
     }
 })
 
+// debug - writing to file?
+console.log(JSON.stringify(results, null, 2))
+
 try
 {
-    // debug
+    debug('Deleting temporary spec file used for remote version')
     fs.unlinkSync(tempSpecFile);
-    console.log(`File "${tempSpecFile}" has been deleted.`);
 } catch (err)
 {
     console.error(`Error deleting file "${tempSpecFile}":`, err);
 }
-
-// debug - writing to file?
-console.log(JSON.stringify(results, null, 2))
