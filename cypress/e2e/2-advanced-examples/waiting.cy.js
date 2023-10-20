878e78c3d13a7cb511cb0ef9eb1d006c59f93a7a
rostyslav znachenko <rostislav.znachenko@gmail.com>
2023-10-20 20:02:24 +0100

    add dummy test

--- cypress/e2e/2-advanced-examples/waiting.cy.js
+++ cypress/e2e/2-advanced-examples/waiting.cy.js
@@ -1,39 +1,40 @@
 /// <reference types="cypress" />
 
-context('Waiting', () =>
-{
-  beforeEach(() =>
-  {
-    cy.visit('https://example.cypress.io/commands/waiting')
-  })
+context("Waiting", () => {
+  beforeEach(() => {
+    cy.visit("https://example.cypress.io/commands/waiting");
+  });
   // BE CAREFUL of adding unnecessary wait times.
   // https://on.cypress.io/best-practices#Unnecessary-Waiting
-  it('cy.wait', () =>
-  {
-    cy.wait(1000)
-  })
+  it("cy.wait", () => {
+    cy.wait(100);
+  });
+
+  it("cy.wait() - 2", () => {
+    cy.wait(500);
+  });
 
   // https://on.cypress.io/wait
-  it('cy.wait() - wait for a specific amount of time', () =>
-  {
-    cy.get('.wait-input1').type('Wait 1000ms after typing')
-    cy.wait(1000)
-    cy.get('.wait-input2').type('Wait 1000ms after typing')
-    cy.wait(1000)
-    cy.get('.wait-input3').type('Wait 1000ms after typing')
-    cy.wait(1000)
-  })
+  it("cy.wait() - wait for a specific amount of time", () => {
+    cy.get(".wait-input1").type("Wait 1000ms after typing");
+    cy.wait(1000);
+    cy.get(".wait-input2").type("Wait 1000ms after typing");
+    cy.wait(1000);
+    cy.get(".wait-input3").type("Wait 1000ms after typing");
+    cy.wait(1000);
+  });
 
-  it('cy.wait() - wait for a specific route', () =>
-  {
+  it("cy.wait() - wait for a specific route", () => {
     // Listen to GET to comments/1
-    cy.intercept('GET', '**/comments/*').as('getComment')
+    cy.intercept("GET", "**/comments/*").as("getComment");
 
     // we have code that gets a comment when
     // the button is clicked in scripts.js
-    cy.get('.network-btn').click()
+    cy.get(".network-btn").click();
 
     // wait for GET comments/1
-    cy.wait('@getComment').its('response.statusCode').should('be.oneOf', [200, 304])
-  })
-})
+    cy.wait("@getComment")
+      .its("response.statusCode")
+      .should("be.oneOf", [200, 304]);
+  });
+});
